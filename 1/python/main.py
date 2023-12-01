a38b79d36a84ab0cc15804211b26cadd4f2c8c44
Alessio Izzo <alessio.izzo86@gmail.com>
2023-12-01 14:19:20 +0100

    solve part 2 of day one

--- 1/python/main.py
+++ 1/python/main.py
@@ -1,10 +1,63 @@
+DIGITS = {
+    "zero": 0,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
 def extract_first_last_digit(input_str: str) -> int:
     first = last = None
+    number = ""
     for char in input_str:
         if char.isdigit():
             if first is None:
                 first = char
             last = char
+        else:
+            number += char
+            for key, value in DIGITS.items():
+                if key in number:
+                    if first is None:
+                        first = value
+                    last = value
+                    number = ""
+
+    return int(f"{first}{last}")
+
+
+def extract_first_last_digit_pointers(input_str: str) -> int:
+    first = last = None
+    number_front = number_back = ""
+    i = 0
+    j = len(input_str)
+    while i <= len(input_str) or j >= 0:
+        if first is None:
+            char_front = input_str[i]
+            if char_front.isdigit():
+                first = char_front
+            else:
+                number_front += char_front
+                for key, value in DIGITS.items():
+                    if key in number_front:
+                        first = value
+        if last is None:
+            char_back = input_str[j - 1]
+            if char_back.isdigit():
+                last = char_back
+            else:
+                number_back = char_back + number_back
+                for key, value in DIGITS.items():
+                    if key in number_back:
+                        last = value
+        i += 1
+        j += -1
 
     return int(f"{first}{last}")
 
@@ -12,7 +65,9 @@ def extract_first_last_digit(input_str: str) -> int:
 def main():
     with open('puzzle_input.txt') as puzzle_input:
         rows = puzzle_input.readlines()
-        result = sum((extract_first_last_digit(row) for row in rows))
+        for row in rows:
+            extract_first_last_digit_pointers(row.replace("\n", ""))
+        result = sum((extract_first_last_digit_pointers(row) for row in rows))
         print(f"The result of the day one of aoc 2023 is {result}")
 
 
--- 1/python/test_main.py
+++ 1/python/test_main.py
@@ -1,5 +1,5 @@
 import pytest
-from main import extract_first_last_digit
+from .main import extract_first_last_digit, extract_first_last_digit_pointers
 
 
 @pytest.mark.parametrize("input_str, expected", [
@@ -8,5 +8,28 @@ from main import extract_first_last_digit
     ("a1b2c3d4e5f", 15),
     ("treb7uchet", 77)
 ])
-def test_extract_first_last_digit(input_str, expected):
+def test_extract_first_last_digit_part_one(input_str, expected):
     assert extract_first_last_digit(input_str) == expected
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    ("1abc2", 12),
+    ("pqr3stu8vwx", 38),
+    ("a1b2c3d4e5f", 15),
+    ("treb7uchet", 77)
+])
+def test_extract_first_last_digit_part_one(input_str, expected):
+    assert extract_first_last_digit_pointers(input_str) == expected
+
+
+@pytest.mark.parametrize("input_str, expected", [
+    ("two1nine", 29),
+    ("eightwothree", 83),
+    ("abcone2threexyz", 13),
+    ("xtwone3four", 24),
+    ("4nineeightseven2", 42),
+    ("zoneight234", 14),
+    ("7pqrstsixteen", 76),
+])
+def test_extract_first_last_digit(input_str, expected):
+    assert extract_first_last_digit_pointers(input_str) == expected
