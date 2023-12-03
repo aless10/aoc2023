bae97fdf44d3bbeeeedb55ce10ae0064d6fdf9bd
Alessio Izzo <alessio.izzo86@gmail.com>
2023-12-03 09:27:49 +0100

    test passing

---
+++ 2/python/main.py
@@ -0,0 +1,44 @@
+LIMITS = {
+    "blue": 14,
+    "green": 13,
+    "red": 12
+}
+
+
+class Extraction:
+    def __init__(self, extraction: dict[str: int]):
+        self.extraction = extraction
+        self.is_valid = self._is_valid()
+
+    @classmethod
+    def from_raw_string(cls, raw_string) -> 'Extraction':
+        args = raw_string.split(",")
+        """3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"""
+        result = {}
+        for arg in args:
+            number, color = arg.strip().split(" ")
+            result[color] = int(number)
+        return cls(result)
+
+    def _is_valid(self):
+        for key, value in self.extraction.items():
+            if value > LIMITS.get(key):
+                return False
+        return True
+
+
+class Game:
+    def __init__(self, id_: str, extractions: list[Extraction]):
+        self.id = id_
+        self.extractions = extractions
+
+    @classmethod
+    def from_raw_line(cls, raw_line):
+        name, extractions = raw_line.split(":")
+        id_ = name.split(" ")[1]
+        extractions = extractions.split(";")
+        extractions = [Extraction.from_raw_string(extraction) for extraction in extractions]
+        return cls(id_, extractions)
+
+    def is_game_possible(self):
+        return all(extraction.is_valid for extraction in self.extractions)
